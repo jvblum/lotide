998f635d30a59514412377916a6b683abe98c1c9
jvblum <jvmell.skch@gmail.com>
2021-09-20 13:01:48 -0400

    ref tail.js; add testTail.js

--- tail.js
+++ tail.js
@@ -1,16 +1,5 @@
-const tail = function(array) {
-  return array.slice(1);
-};
+const assertEqual = require("./assertEqual");
 
-//
+const tail = array => array.slice(1);
 
-const assertEqual = function(actual, expected) {
-  let evaluate;
-  actual === expected ? evaluate = "Passed ✅" : evaluate = "Failed 🔴";
-  console.log(`Assertion ${evaluate}: ${actual} === ${expected}`);
-};
-
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3);
-
+module.exports = tail;
