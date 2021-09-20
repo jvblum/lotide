50a0642bf9ff9008096ab23a20f5612981e2041c
jvblum <jvmell.skch@gmail.com>
2021-09-20 13:55:25 -0400

    ref assertArraysEqual.js (add modules.export); mv test cases to assertArrayEqualTest.js

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,24 +1,8 @@
-const eqArrays = function(array1, array2) {
-  let mismatch;
-  for (let i = 0; i <= array1.length; i++) { // "<=" for cases when array1[i] is equal to array2[i] within array1.length, but array1.length !== array2.length
-    if (array1[i] !== array2[i]) {
-      // console.log("mismatch!", array1[i], "?==", array2[i]);
-      mismatch = true;
-      break;
-    }
-  }
-  return mismatch ? false : true;
-};
+const eqArrays = require("./eqArrays");
 
 const assertArraysEqual = function(array1, array2) {
-  if (eqArrays(array1, array2)) {
-    console.log(`Assertion Passed✅`);
-  } else {
-    console.log(`Assertion Failed🔴`);
-  }
+  if (eqArrays(array1, array2)) return console.log(`Assertion Passed✅: [${array1}] === [${array2}]`);
+  return console.log(`Assertion Failed🔴: [${array1}] === [${array2}]`);
 };
 
-// test
-
-assertArraysEqual([1, 2, 3], [1, 2, 3]); 
-assertArraysEqual([1, 2, 3], [1, 2, 3, 4]); 
+module.exports = assertArraysEqual;
